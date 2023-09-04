3791c09acf8b5700356e0673aaa604e75ce5c8c3
krutikpatel00 <krutikpatel0214@gmail.com>
2023-09-04 10:05:08 +0530

    pr-6

---
+++ js/eduvibe.js
@@ -0,0 +1,130 @@
+$('.owl-carousel-1').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+})
+
+$(document).ready(function () {
+  $(".toggle-manu").click(function (e) {
+    $("nav").toggleClass("nav-open");
+  });
+
+
+  $(".back-to-top").click(function (e) {
+    $("html").animate({ "scrollTop": "0" });
+
+  });
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 0) {
+    $("header").addClass("active-header");
+    $(".back-to-top").fadeIn();
+  }
+
+
+  else {
+    $("header").removeClass("active-header");
+    $(".back-to-top").fadeOut();
+  }
+
+});
+$(window).on("load", function () {
+  $(".loader").fadeOut(2000);
+});
+
+// gallery
+
+filterSelection("all") // Execute the function and show all 
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+// couter
+var a = 0;
+$(window).scroll(function () {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function () {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum: countTo
+      },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
